9c5c8fd5ae5c8007b4ae93782b53cf219dba10de
distributed-memory-9073 <noahshafer0211@gmail.com>
2020-04-01 20:09:07 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,4 @@
 def find_even_values(src)
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
-  src.each {|arr| arr.forEach { |a| puts a if a.modulo(2) == 0 } }
+  src.each {|arr| arr.each { |a| puts a if a.modulo(2) == 0 } }
 end
